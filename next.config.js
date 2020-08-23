40e0347492660710fd8f74210c02fa3fd4548570
jioo <jio.qzn@gmail.com>
2020-08-23 13:04:10 +0800

    Updated public path prefix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 module.exports = {
+  env: {
+    BACKEND_URL: (process.env.NODE_ENV === 'production') ? '/nextjs-api-ghpage-test' : ''
+  },
   assetPrefix: (process.env.NODE_ENV === 'production') ? '/nextjs-api-ghpage-test' : '',
-  // webpack: (config, { dev }) => {
-  //   // Perform customizations to webpack config
-  //   config.module.rules = config.module.rules.map(rule => {
-  //     if(rule.loader === 'babel-loader') {
-  //       rule.options.cacheDirectory = false
-  //     }
-  //     return rule
-  //   })
-  //   // Important: return the modified config
-  //   return config
-  // },
-  webpack: (config) => {
-    config.output.publicPath = `/nextjs-api-ghpage-test${config.output.publicPath}`;
-    return config;
-  }
+  // webpack: (config) => {
+  //   config.output.publicPath = `/nextjs-api-ghpage-test${config.output.publicPath}`;
+  //   return config;
+  // }
 }
