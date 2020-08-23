18fd87d424c204f969e90dbb60888a1c6da2aabb
jioo <jio.qzn@gmail.com>
2020-08-23 12:27:09 +0800

    Updated next.config to apply assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  assetPrefix: (process.env.NODE_ENV === 'production') ? '/nextjs-api-ghpage-test' : ''
+  assetPrefix: (process.env.NODE_ENV === 'production') ? '/nextjs-api-ghpage-test' : '',
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    // Important: return the modified config
+    return config
+  }
 }
